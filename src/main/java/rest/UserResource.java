7bc33930111fc95bdf86f6a7c4901f11ea53f03a
Gustav <cph-gw30@cphbusiness.dk>
2020-11-11 06:13:56 +0100

    up

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -8,12 +8,20 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dtos.UsersDTO;
+import entities.User;
 import facades.UserFacade;
+import java.util.List;
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
 import utils.EMF_Creator;
 
 /**
@@ -23,13 +31,19 @@ import utils.EMF_Creator;
  */
 
 
-@Path("user")
+@Path("info")
 public class UserResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
        
     private static final UserFacade FACADE =  UserFacade.getUserFacade(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
     
     
     @GET
@@ -56,7 +70,37 @@ public class UserResource {
         return GSON.toJson(users);
     }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("allCount")
+    public String allUsers() {
+
+        EntityManager em = EMF.createEntityManager();
+        try {
+            TypedQuery<User> query = em.createQuery ("select u from User u",entities.User.class);
+            List<User> users = query.getResultList();
+            return "[" + users.size() + "]";
+        } finally {
+            em.close();
+        }
+    }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed({"user", "admin"})
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
+    }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
+    }
 }
 
--- src/test/java/facades/UserFacadeTest.java
+++ src/test/java/facades/UserFacadeTest.java
@@ -15,50 +15,50 @@ import org.junit.jupiter.api.Test;
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
 public class UserFacadeTest {
-
-    private static EntityManagerFactory emf;
-    private static UserFacade facade;
-
-    public UserFacadeTest() {
-    }
-
-    @BeforeAll
-    public static void setUpClass() {
-       emf = EMF_Creator.createEntityManagerFactoryForTest();
-       facade = UserFacade.getUserFacade(emf);
-    }
-
-    @AfterAll
-    public static void tearDownClass() {
-//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
-    }
-
-    // Setup the DataBase in a known state BEFORE EACH TEST
-    //TODO -- Make sure to change the code below to use YOUR OWN entity class
-    @BeforeEach
-    public void setUp() {
-        EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.createNamedQuery("Users.deleteAllRows").executeUpdate();
-            em.persist(new User("Some txt", "More text"));
-            em.persist(new User("aaa", "bbb"));
-
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
-    }
-
-    @AfterEach
-    public void tearDown() {
-//        Remove any data after each test was run
-    }
-
-    // TODO: Delete or change this method 
-    @Test
-    public void testAFacadeMethod() {
-        assertEquals(2, facade.getUserCount(), "Expects two rows in the database");
-    }
+//
+//    private static EntityManagerFactory emf;
+//    private static UserFacade facade;
+//
+//    public UserFacadeTest() {
+//    }
+//
+//    @BeforeAll
+//    public static void setUpClass() {
+//       emf = EMF_Creator.createEntityManagerFactoryForTest();
+//       facade = UserFacade.getUserFacade(emf);
+//    }
+//
+//    @AfterAll
+//    public static void tearDownClass() {
+////        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
+//    }
+//
+//    // Setup the DataBase in a known state BEFORE EACH TEST
+//    //TODO -- Make sure to change the code below to use YOUR OWN entity class
+//    @BeforeEach
+//    public void setUp() {
+//        EntityManager em = emf.createEntityManager();
+//        try {
+//            em.getTransaction().begin();
+//            em.createNamedQuery("Users.deleteAllRows").executeUpdate();
+//            em.persist(new User("Some txt", "More text"));
+//            em.persist(new User("aaa", "bbb"));
+//
+//            em.getTransaction().commit();
+//        } finally {
+//            em.close();
+//        }
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+////        Remove any data after each test was run
+//    }
+//
+//    // TODO: Delete or change this method 
+//    @Test
+//    public void testAFacadeMethod() {
+//        assertEquals(2, facade.getUserCount(), "Expects two rows in the database");
+//    }
 
 }
